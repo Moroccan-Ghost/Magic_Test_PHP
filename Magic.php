0303958523d335d57a11908319cdf0a988c9e3d5
Moroccan-Ghost <aiman.elbouayadi@gmail.com>
2022-12-13 20:57:10 +0100

    reduce code

--- Magic.php
+++ Magic.php
@@ -1,7 +1,7 @@
 <?php
 
-$value = 1114;
-$stat = "iNc";
+$value = -5;
+$stat = "dec";
 
 $result = magic_inc($value, $stat);
 echo 'Result for ' . $value . ' is : ' . $result;
@@ -10,20 +10,14 @@ function magic_inc($value, $stat)
     if ((gettype($value) != "integer" && gettype($value) != "double") || $value == 0) {
         return 0;
     } else {
-        //calculateResult($value,$stat);
-        if (strtolower($stat) == "inc") {
-            return inc($value);
-        }
-        if (strtolower($stat) == "dec") {
-            return dec($value);
-        }
+        return calculateResult($value,$stat);
     }
 }
 function calculateResult($nbr,$stat){
     if ($nbr > 0) {
-        if ($nbr > 0 && $nbr < 1) {
+        if ($nbr < 1) {
             $result = $nbr * 10;
-            /**Divise to get the real part of nbr number */
+            /**Divide to get the real part of nbr number */
             $i = 0.1;
             while ($result < 1) {
                 $result = $result * 10;
@@ -39,7 +33,7 @@ function calculateResult($nbr,$stat){
             }
 
         }
-        /**if nbr is btwn 1 and 9 */
+        /**if nbr is between 1 and 9 */
         if ($nbr >= 1 && $nbr < 10) {
             if (strtolower($stat) == "inc") {
                 return floor($nbr) + 1;
@@ -57,7 +51,7 @@ function calculateResult($nbr,$stat){
                 $i = $i * 10;
             }
             $valueToIncrementWith = $nbr / $result;
-            //i added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
+            //I added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
             $nbr = intval($nbr / $i) * $i;
             if (strtolower($stat) == "inc") {
                 return $nbr + $valueToIncrementWith;
@@ -87,7 +81,7 @@ function calculateResult($nbr,$stat){
             }
         }
 
-        /**if nbr is btwn 1 and 9 */
+        /**if nbr is between 1 and 9 */
         if ($nbr > -10 && $nbr <= -1) {
             if (strtolower($stat) == "inc") {
                 return ceil($nbr) + 1;
@@ -98,14 +92,14 @@ function calculateResult($nbr,$stat){
         }
         if ($nbr < -10) {
             $result = $nbr / 10;
-            /**Divise to get the real part of nbr number */
+            /**Divide to get the real part of nbr number */
             $i = 10;
             while ($result < -10) {
                 $result = $result / 10;
                 $i = $i * 10;
             }
             $valueToIncrementWith = $nbr / $result;
-            //i added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
+            //I added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
             $nbr = intval($nbr / $i) * $i;
             if (strtolower($stat) == "inc") {
                 return $nbr + $valueToIncrementWith;
@@ -116,151 +110,4 @@ function calculateResult($nbr,$stat){
         }
     }
 }
-function inc($nbr)
-{
-    if ($nbr > 0) {
-        if ($nbr > 0 && $nbr < 1) {
-            $result = $nbr * 10;
-            /**Divise to get the real part of nbr number */
-            $i = 0.1;
-            while ($result < 1) {
-                $result = $result * 10;
-                $i = $i / 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr + $valueToIncrementWith;
-        }
-
-        /**if nbr is btwn 1 and 9 */
-        if ($nbr >= 1 && $nbr < 10) {
-            return floor($nbr) + 1;
-        }
-        if ($nbr >= 10) {
-            $result = $nbr / 10;
-            /**Divide to get the real part of nbr number */
-            $i = 10;
-            while ($result >= 10) {
-                $result = $result / 10;
-                $i = $i * 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            //i added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr + $valueToIncrementWith;
-        }
-    }
-    else {
-        if ($nbr > -1 && $nbr < 0) {
-            $result = $nbr * 10;
-            /**Divide to get the real part of nbr number */
-            $i = 0.1;
-            while ($result > -1) {
-                $result = $result * 10;
-                $i = $i / 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr + $valueToIncrementWith;
-        }
-
-        /**if nbr is btwn 1 and 9 */
-        if ($nbr > -10 && $nbr <= -1) {
-            return ceil($nbr) + 1;
-        }
-        if ($nbr < -10) {
-            $result = $nbr / 10;
-            /**Divise to get the real part of nbr number */
-            $i = 10;
-            while ($result < -10) {
-                $result = $result / 10;
-                $i = $i * 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            //i added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr + $valueToIncrementWith;
-        }
-    }
-}
-
-function dec($nbr)
-{
-    if ($nbr < 0) {
-        if ($nbr > -1 && $nbr < 0) {
-            $result = $nbr * 10;
-            /**Divise to get the real part of nbr number */
-            $i = 0.1;
-            while ($result > -1) {
-                $result = $result * 10;
-                $i = $i / 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr + $valueToIncrementWith;
-        }
-        if ($nbr > -1) {
-            $result = $nbr * 10;
-            /**Divide to get the real part of nbr number */
-            $i = 0.1;
-            while ($result > -1) {
-                $result = $result * 10;
-                $i = $i / 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr - $valueToIncrementWith;
-        }
-
-        /**if nbr is between 1 and 9 */
-        if ($nbr > -10 && $nbr <= -1) {
-            return ceil($nbr) - 1;
-        }
-        if ($nbr <= -10) {
-            $result = $nbr / 10;
-            /**Divide to get the real part of nbr number */
-            $i = 10;
-            while ($result <= -10) {
-                $result = $result / 10;
-                $i = $i * 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            //I added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr - $valueToIncrementWith;
-        }
-    } else {
-        if ($nbr > 0 && $nbr < 1) {
-            $result = $nbr * 10;
-            /**Divide to get the real part of nbr number */
-            $i = 0.1;
-            while ($result <= 1) {
-                $result = $result * 10;
-                $i = $i / 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr - $valueToIncrementWith;
-        }
-
-        /**if nbr is btwn 1 and 9 */
-        if ($nbr >= 1 && $nbr < 10) {
-            return floor($nbr) - 1;
-        }
-        if ($nbr >= 10) {
-            $result = $nbr / 10;
-            /**Divide to get the real part of nbr number */
-            $i = 10;
-            while ($result >= 10) {
-                $result = $result / 10;
-                $i = $i * 10;
-            }
-            $valueToIncrementWith = $nbr / $result;
-            //i added the i counter to get the reel part of nbr and then add the valueToIncrementWith to it after :)
-            $nbr = intval($nbr / $i) * $i;
-            return $nbr - $valueToIncrementWith;
-        }
-    }
-}
-
 ?>
